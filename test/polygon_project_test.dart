a867636442936a39d990b37c9f00655d8ad678a9
sanjarbek <sanjarsaidov173@gmail.com>
2022-09-19 15:39:04 +0500

    test

--- test/polygon_project_test.dart
+++ test/polygon_project_test.dart
@@ -3,7 +3,7 @@ import 'package:polygon_project/circle.dart';
 
 void main() {
   double r = 10;
-  Circle circle = Circle();
+  Circle circle = Circle(r);
   test('Circle circumference', () {
     expect(circle.circumference(), 2 * 3.1415926535897932 * r);
   });
